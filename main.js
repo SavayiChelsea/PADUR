6aa2d44b744006b586d7bede78ebf21d24c8d2b5
SavayiChelsea <savayichelsea90@gmail.com>
2025-06-26 17:12:26 +0300

    projects section

---
+++ main.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    let swiper = new Swiper(".swiper .container", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        grabCursor: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true
+        }
+    });
+});
